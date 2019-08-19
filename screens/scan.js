8a38a29084e876e62c2c1c779c769fec4d0378ea
shadman96 <mdsadman96@gmail.com>
2019-08-19 15:27:35 +0530

    css fixes

--- screens/scan.js
+++ screens/scan.js
@@ -1,25 +1,19 @@
 import React, { Component, Fragment } from 'react';
 import QRCodeScanner from 'react-native-qrcode-scanner';
-
+import styles from './scanStyle'
 import {
-    SafeAreaView,
-    StyleSheet,
-    ScrollView,
     TouchableOpacity,
     Text,
     StatusBar,
     Linking,
-    View,
-    Dimensions,
-    Button
+    View
 } from 'react-native';
 
 import {
     Header,
     Colors,
 } from 'react-native/Libraries/NewAppScreen';
-const deviceWidth = Dimensions.get('screen').width;
-const deviceHeight = Dimensions.get('screen').height;
+
 class Scan extends Component {
     constructor(props) {
         super(props);
@@ -130,116 +124,6 @@ class Scan extends Component {
     }
 }
 
-const styles = StyleSheet.create({
-    scrollViewStyle: {
-        // height:deviceHeight,
-        flex: 1,
-        justifyContent: 'center',
-        backgroundColor: '#99003d'
-    },
 
-    textTitle: {
-        fontWeight: 'bold',
-        fontSize: 18,
-        textAlign: 'center',
-        padding: 16,
-        color: 'white'
-    },
-    cardView: {
-        width: deviceWidth - 32,
-        height: deviceHeight / 2,
-        alignSelf: 'center',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        borderWidth: 1,
-        borderRadius: 2,
-        borderColor: '#ddd',
-        borderBottomWidth: 0,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.8,
-        shadowRadius: 2,
-        elevation: 4,
-        marginLeft: 5,
-        marginRight: 5,
-        marginTop: 10,
-        backgroundColor: 'white'
-    },
-    scanCardView: {
-        width: deviceWidth - 32,
-        height: deviceHeight / 2,
-        alignSelf: 'center',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderWidth: 1,
-        borderRadius: 2,
-        borderColor: '#ddd',
-        borderBottomWidth: 0,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.8,
-        shadowRadius: 2,
-        elevation: 4,
-        marginLeft: 5,
-        marginRight: 5,
-        marginTop: 10,
-        backgroundColor: 'white'
-    },
-    buttonScan: {
-        width: 42,
-
-    },
-    descText: {
-        padding: 16,
-        textAlign: 'justify',
-        fontSize: 16
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-        color: Colors.black,
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-        color: Colors.dark,
-    },
-    highlight: {
-        fontWeight: '700',
-    },
-    footer: {
-        color: Colors.dark,
-        fontSize: 12,
-        fontWeight: '600',
-        padding: 4,
-        paddingRight: 12,
-        textAlign: 'right',
-    },
-    centerText: {
-        flex: 1,
-        fontSize: 18,
-        padding: 32,
-        color: '#777',
-    },
-    textBold: {
-        fontWeight: '500',
-        color: '#000',
-    },
-    buttonTouchable: {
-        fontSize: 21,
-        backgroundColor: '#ff0066',
-        marginTop: 32,
-
-        width: deviceWidth - 62,
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 44
-    },
-    buttonTextStyle: {
-        color: 'white',
-        fontWeight: 'bold',
-    }
-});
 
 export default Scan;
